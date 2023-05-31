6bfa3bf437a2f75b63a6b30685b91e5d70a9a034
Guillaume G <suricator@free.fr>
2023-05-31 15:15:51 +0100

    Update hardhat.config.ts

--- GBM-Diamond/hardhat.config.ts
+++ GBM-Diamond/hardhat.config.ts
@@ -6,6 +6,10 @@ const config: HardhatUserConfig = {
   defaultNetwork: "actualHardhat",
   networks: {
     hardhat: {
+      mining: {
+        auto: true,
+        interval: [3000, 6000]
+      }
     }, 
     actualHardhat:{
       url: "http://127.0.0.1:8545"
