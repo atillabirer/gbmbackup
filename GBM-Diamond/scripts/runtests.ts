34f1552a67aa274b00b954c1e7307b2eb73e9560
Guillaume G <suricator@free.fr>
2023-05-31 14:10:21 +0100

    Editing conf

--- GBM-Diamond/scripts/runtests.ts
+++ GBM-Diamond/scripts/runtests.ts
@@ -2,6 +2,8 @@ let deploy = require("./deploy.ts");
 
 let conf = JSON.parse(require("../gbm.config.ts").conf);
 
+const AUTOMATED_TEST:boolean = true;
+
 if (true) {
-    deploy.main();
+    deploy.main(true);
 }
