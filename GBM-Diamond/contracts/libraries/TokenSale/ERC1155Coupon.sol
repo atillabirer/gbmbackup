842b36d689e60b9a0a1df8d728dae6105fd23969
Guillaume G <suricator@free.fr>
2023-06-19 11:26:13 +0100

    1155 solidity fixed

--- GBM-Diamond/contracts/libraries/TokenSale/ERC1155Coupon.sol
+++ GBM-Diamond/contracts/libraries/TokenSale/ERC1155Coupon.sol
@@ -107,6 +107,7 @@ contract ERC1155Coupon is IERC1155, IERC165 {
             //Adjusting the balances
             balanceOfVar[_from][_ids[tmp]] = balanceOfVar[_from][_ids[tmp]] - _values[tmp];
             balanceOfVar[_to][_ids[tmp]] = balanceOfVar[_to][_ids[tmp]] + _values[tmp];
+            tmp++:
         }
 
         //Emitting the event
@@ -132,7 +133,7 @@ contract ERC1155Coupon is IERC1155, IERC165 {
     /// @notice Get the balance of multiple account/token pairs
     /// @param _owners The addresses of the token holders
     /// @param _ids    ID of the tokens
-    /// @return        The _owner's balance of the token types requested (i.e. balance for each (owner, id) pair)
+    /// @return balances       The _owner's balance of the token types requested (i.e. balance for each (owner, id) pair)
     function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external override view returns (uint256[] memory){
 
         //Making value readable from stack directly instead of having to read the stack as pointer to get length = less gas when looping
@@ -265,9 +266,5 @@ contract ERC1155Coupon is IERC1155, IERC165 {
         assembly { codehash := extcodehash(_address) }
         return (codehash != accountHash && codehash != 0x0);
     }
-
-
-
-
 
 }
