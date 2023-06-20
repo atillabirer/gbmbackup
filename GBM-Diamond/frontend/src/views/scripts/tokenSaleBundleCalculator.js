41656cdcc3934a7c43e14ea8b891100afce4b3a5
Guillaume G <suricator@free.fr>
2023-06-20 01:43:10 +0100

    Update tokenSaleBundleCalculator.js

--- GBM-Diamond/frontend/src/views/scripts/tokenSaleBundleCalculator.js
+++ GBM-Diamond/frontend/src/views/scripts/tokenSaleBundleCalculator.js
@@ -6,7 +6,7 @@ Basically a bank note distribution between the number of 0 and the face (1, 2, 5
 
  */
 
-function get_bundles(_minCoeff, _minExp, _maxCoeff, _maxExp) {
+function get_bundlesMath(_minCoeff, _minExp, _maxCoeff, _maxExp) {
     let faces = [1, 2, 5];
 
     if (faces.indexOf(_minCoeff) == -1) {
@@ -23,16 +23,15 @@ function get_bundles(_minCoeff, _minExp, _maxCoeff, _maxExp) {
     let distrib = [];
 
     if (range > 0) {
-        //Adding the lower values of faces
-        for (let i = faces.indexOf(_minCoeff); i < 3; i++) {
+
+        for (let i = faces.indexOf(_minCoeff); i < 3; i++) {         //Adding the lower values of faces
             distrib.push(faces[i] * multiplier);
         }
 
         multiplier = multiplier * 10;
 
-        //adding the mid values of faces
-        let i = 1;
-        while (i < range) {
+        let i = 1;   
+        while (i < range) {  //adding the mid values of faces
             distrib.push(faces[0] * multiplier);
             distrib.push(faces[1] * multiplier);
             distrib.push(faces[2] * multiplier);
@@ -55,4 +54,70 @@ function get_bundles(_minCoeff, _minExp, _maxCoeff, _maxExp) {
 
     return distrib;
 
+}
+
+// _totalTokens : float
+// _distrib : array of banknote values (1, 2, 5, 10...)
+// _whale_factor : a float between 0 and 1. If it's close to 1 : most of the value is in as high banknote as possible. If close to 0, in as small banknots as possible
+// Return : ( [{value:"banknoteValue", amount:"amountOfThisBanknotTosell"},...], number of undistributed tokens);
+function getSalesDistributionMath(_totalTokens, _distrib, _whale_factor){
+
+    if(_whale_factor < 0 || _whale_factor > 1){
+        throw ("Wrong _whale_factor. Must be betwen 0.0 and 1.0");
+    }
+
+    let reminder_tokens = _totalTokens;
+    let saleBundling = [];
+    console.log(_distrib);
+    for(let i = _distrib.length-1; i >= 0; i--){
+        let localAmount = Math.floor( //Must be an integer
+                _whale_factor * ( //Control how top-heavy the distribution is
+                        reminder_tokens / _distrib[i]
+                    )
+            );
+        saleBundling.push({"value":_distrib[i], "amount":localAmount});
+        reminder_tokens -= localAmount * _distrib[i];
+    }
+
+    //Adding the leftover to the smallest banknote
+    if(reminder_tokens != 0){
+        let localAmount = Math.floor( reminder_tokens / _distrib[0]) ;
+        saleBundling[saleBundling.length -1] = {"value":_distrib[0], "amount":(localAmount + saleBundling[saleBundling.length -1].amount)};
+        reminder_tokens -= localAmount;
+    }
+      
+    return(saleBundling, reminder_tokens);
+}
+
+
+// Pretty sure there's a more elegant way to do this
+// Convert 500 to (5, 2)  or 0.2 to (2, -1)
+function toCoeffAndExp(_input){
+    _input = Math.log10(_input);
+    let _exp = Math.floor(_input);
+    let _coef;
+
+    switch(Math.floor(_input * 10)%10){
+        case 0:
+            _coef = 1;
+            break;
+        case 3:
+            _coef = 2;
+            break;
+        case 6: 
+            _coef = 5;
+            break;
+        default:
+            _coef = 1;
+    }
+
+    return ({_coef, _exp});
+}
+
+//   return(saleBundling, reminder_tokens);
+function generateDistributionFromNotesAndTokenAmount(_totalTokens, _smallestNote, _biggestNote, _whaleFactor){
+    let min = toCoeffAndExp(_smallestNote);
+    let max = toCoeffAndExp(_biggestNote);
+    let _distrib = get_bundlesMath(min._coef, min._exp, max._coef, max._exp);
+    return getSalesDistributionMath(_totalTokens, _distrib, _whaleFactor);
 }
